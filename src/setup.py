5a19eee7ad3d2c6efcc78839c18dd9d7152e96d8
Robert Gieseke <rob.g@web.de>
2021-08-03 15:02:09 +0200

    Blacken

--- src/setup.py
+++ src/setup.py
@@ -17,34 +17,39 @@ import versioneer
 
 path = os.path.abspath(os.path.dirname(__file__))
 
-description = 'Global warming potentials of greenhouse gases from various IPCC reports'
+description = "Global warming potentials of greenhouse gases from various IPCC reports"
 
-with open(os.path.join(path, '../README.md'), "r") as f:
+with open(os.path.join(path, "../README.md"), "r") as f:
     readme = f.read()
 
 cmdclass = versioneer.get_cmdclass()
 
 setup(
-    name='globalwarmingpotentials',
+    name="globalwarmingpotentials",
     version=versioneer.get_version(),
     description=description,
     long_description=readme,
-    long_description_content_type='text/markdown',
-    url='https://github.com/openclimatedata/globalwarmingpotentials',
-    author='Robert Gieseke',
-    author_email='rob.g@web.de',
-    license='CC0',
-    platforms='any',
+    long_description_content_type="text/markdown",
+    url="https://github.com/openclimatedata/globalwarmingpotentials",
+    author="Robert Gieseke",
+    author_email="rob.g@web.de",
+    license="CC0",
+    platforms="any",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    keywords=['climate research', 'greenhouse gases', 'global warming potentials', 'climate change'],
+    keywords=[
+        "climate research",
+        "greenhouse gases",
+        "global warming potentials",
+        "climate change",
+    ],
     cmdclass=cmdclass,
-    packages=['globalwarmingpotentials']
+    packages=["globalwarmingpotentials"],
 )
