e962d42762f51676cf680aae4354318913b88a81
Robert Gieseke <rob.g@web.de>
2021-08-03 14:30:16 +0200

    Add tag generation script

---
+++ scripts/create_tag.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+sed="sed";
+uname=$(uname);
+case "$uname" in
+    (*Darwin*) sed='gsed'; ;;
+esac;
+
+
+if [[ "$(git status --porcelain)" ]]; then
+  printf "Working directory not clean:\n\n"
+  git status
+else
+  echo Current tags:
+  git tag
+
+  echo Commits since last tag:
+  git log "$(git describe --tags --abbrev=0)"..HEAD --oneline
+  echo "Please enter next version (x.y.z):"
+  read version
+  echo "You entered: $version"
+  "$sed" -i -r "s/(\"version\": \").*(\")/\1$version\2/" datapackage.json
+  git add datapackage.json
+  git commit -m "Update version in datapackage.json"
+  npm version "$version" -m "Update version to %s"
+  git add package.json
+  git commit -m "Update version in package.json"
+  git tag v$version
+fi
