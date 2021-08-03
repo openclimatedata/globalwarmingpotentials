4e42822fd949a5656152b5878cff2de96630b4af
Robert Gieseke <rob.g@web.de>
2021-08-03 13:28:22 +0200

    Fix Data Package and add validation script

---
+++ scripts/validate.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from goodtables import validate
+from goodtables.cli import _print_report
+
+root = Path(__file__).parents[1]
+
+report = validate(
+    str(root / "datapackage.json")
+)
+_print_report(report)
