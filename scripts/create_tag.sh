61e7cc3bd9ba74d408bdb697ae3fc3e53626964a
Robert Gieseke <rob.g@web.de>
2021-08-03 20:30:48 +0200

    Fix tag generation script

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -18,9 +18,5 @@ else
   git log "$(git describe --tags --abbrev=0)"..HEAD --oneline
   echo "Please enter next version (x.y.z):"
   read version
-  echo "You entered: $version"
-  "$sed" -i -r "s/(\"version\": \").*(\")/\1$version\2/" datapackage.json
-  git add datapackage.json
-  git commit -m "Update version in datapackage.json"
   npm version "$version" -m "Update version to %s"
 fi
